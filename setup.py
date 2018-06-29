94093863f7b99f740acd67607586828dc6707862
Ухина Ольга Валерьевна <ov_uhina@bingo-boom.ru>
2018-06-29 15:27:13 +0300

    Change name of the package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ __version__ = "0.0.1"
 
 setup(
     version=__version__,
-    name="Ferment",
+    name="ferment_ng",
     description="A tool to provide the current docker config in ferm format",
     author="Olga Ukhina",
     author_email="olga.uhina@gmail.com",
@@ -22,12 +22,12 @@ setup(
         "src",
         exclude=[]
     ),
-    namespace_packages=["ferment-ng"],
+    namespace_packages=["ferment_ng"],
     include_package_data=True,
     zip_safe=False,
     entry_points="""\
     [console_scripts]
-    ferment-ng = ferment-ng.scripts:run
+    ferment_ng=ferment_ng.scripts:run
     """,
     install_requires=[
         "docker-py",
